f10224ddb5899b7ee7dd5eebeb5ffd71659032e0
Ubuntu <techme@mostlyevents.com>
2025-07-16 05:04:44 +0000

    deployement setup

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'resume-viewer',
+      script: 'npm',
+      args: 'start',
+      instances: 'max',
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      }
+    }
+  ]
+}
